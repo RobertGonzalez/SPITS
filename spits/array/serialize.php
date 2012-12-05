61dd0d30e5f2e24dd49755a44194b578481c058f
Robert Gonzalez <robert@robert-gonzalez.com>
2012-12-04 21:44:33 -0800

    Add the serialize test (I think anyway)

--- spits/array/serialize.php
+++ spits/array/serialize.php
@@ -1,9 +1,26 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: admin
- * Date: 5/28/12
- * Time: 8:26 PM
- * To change this template use File | Settings | File Templates.
- */
- 
+$pagetitle = 'Array (Un)Serializer';
+
+$i = empty($_POST['input']) ? null : $_POST['input'];
+$o = null;
+$s = !empty($_POST['serialize']);
+
+if ($i) {
+	$func = 'serialize';
+	if (!$s) $func = 'un' . $func;
+	$o = $func($i);
+}
+?>
+<form id="regex_form" method="post" action="">
+Input: <textarea name="input" rows="5" style="width:100%;"><?php echo $i ?></textarea>
+<br />
+Serialize: <input type="checkbox" name="serialize"<?php if($s): ?> checked="checked"<?php endif; ?> />
+<br /><br />
+<input type="submit" name="submit" value="Process" />
+</form>
+<?php if ($o): ?>
+<hr />
+Output:<br /><pre>
+<?php var_dump($o) ?> 
+</pre>
+<?php endif; ?> 
